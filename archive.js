fc9dcb0da5925d6b8e086fa037f86eda3d86e57a
PAIDI NAVEEN <naveen07paidi@gmail.com>
2025-09-15 05:48:45 +0530

    Add files via upload

---
+++ archive.js
@@ -0,0 +1,32 @@
+import { renderNotes } from "./app.js";
+
+let arrayOfNotes = JSON.parse(localStorage.getItem("notes")) || [];
+
+let showArchiveNotes = document.querySelector(".archive-notes-container");
+
+window.addEventListener("storage", () => {
+    arrayOfNotes = JSON.parse(localStorage.getItem("notes")) || [];
+    showArchiveNotes.innerHTML = renderNotes(
+        arrayOfNotes.filter(({ isArchived }) => isArchived)
+    );
+});
+
+showArchiveNotes.addEventListener("click", (event) => {
+    let type = event.target.dataset.type;
+    let noteId = event.target.dataset.id;
+    
+    switch(type) {
+        case "del":
+            arrayOfNotes = arrayOfNotes.filter(({id}) => id.toString() !== noteId);
+            showArchiveNotes.innerHTML = renderNotes(arrayOfNotes.filter(({isArchived}) => isArchived));
+            localStorage.setItem("notes", JSON.stringify(arrayOfNotes));
+            break;
+        case "archive":
+            arrayOfNotes = arrayOfNotes.map(note => note.id.toString() === noteId ? {...note, isArchived: !note.isArchived}: note);
+            showArchiveNotes.innerHTML = renderNotes(arrayOfNotes.filter(({isArchived}) => isArchived));
+            localStorage.setItem("notes", JSON.stringify(arrayOfNotes));
+            break;
+    }
+});
+
+showArchiveNotes.innerHTML = renderNotes(arrayOfNotes.filter(({isArchived}) => isArchived));
